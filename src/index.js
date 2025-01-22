35e5e72a7d07651ce072a5fbbe10e2bebb987f3e
amazing <amazing@fifteenh.io>
2025-01-22 23:52:05 +0900

    준비중

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Wedding from './wedding';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Wedding />
+  </React.StrictMode>
+);
---
+++ src/wedding.js
@@ -0,0 +1,11 @@
+import './css/style.css'
+
+function Wedding() {
+    return (
+        <div className="mainBox">
+            <img src="/assets/images/main.png"/>
+        </div>
+    );
+}
+
+export default Wedding;
